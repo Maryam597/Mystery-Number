3b465c61cbe1ae08206af56bacbf9730f9790135
Maryam597 <maryam.dri@hotmail.fr>
2023-06-26 09:22:51 +0200

    Mystery Number

---
+++ script.js
@@ -0,0 +1,11 @@
+let button = document.getElementById('button'); // creating two variables
+let outputtext = document.getElementById('outputtext');
+
+let number = [MAth.floor(Math.random() * 100)] // creating a variable which just holds a number
+
+button.addEventListener('click', function() {
+
+    {
+        alert ("Hello World!");
+      }
+})
