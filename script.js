d29fc956c180abd3ffbf901641d4097cdd71653b
Maryam597 <maryam.dri@hotmail.fr>
2023-06-27 14:51:05 +0200

    Almost done

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ let chances = 1;
 let number = Math.floor(Math.random() * 10); // creating a variable which just holds a number
 console.log(number);
 
-// let replay = onClick=window.location.reload()
-
+function refresh () {
+    location.reload ()
+}
 
 button.addEventListener('click', function() {
+
+
+    if (chances <= 3){
 
     if (input.value < number) {
         outputtext.innerHTML += `<hr><h3 class="text-center"> ${chances}er Essai </h3><p class="text-center"> ${input.value}? ...c'est ➕</p>`
@@ -23,33 +27,23 @@ button.addEventListener('click', function() {
   
 
     else if  (input.value = number){
-        outputtext.innerHTML += `<hr><h3 class="text-center"> ${chances}ème Essai </h3><p class="text-center">  <h2> Gagné! 🙂</h2>  numéro mystère : ${number}</p>`;
+        outputtext.innerHTML += `<hr><h3 class="text-center"> ${chances}ème Essai </h3>  <h2 class="text-center"> Gagné! 🙂</h2> <p class="text-center"> numéro mystère : ${number}</p>
+        <div class="text-center"> <button class="btn btn-primary" type="submit" onclick="refresh()" >Rejouer</button> </div>`;
+        
     }
 
     console.log(chances);
 
     if (chances === 3 && input.value != number ) {
-        outputtext.innerHTML += `<h2 class="text-center"> Perdu! ☹️</h2> <p class="text-center"> numéro mystère : ${number}</p>`
+        outputtext.innerHTML += `<h2 class="text-center"> Perdu! ☹️</h2> <p class="text-center"> numéro mystère : ${number}</p> 
+       <div class="text-center"> <button class="btn btn-primary" type="submit" onclick="refresh()" >Rejouer</button> </div> `
+         
 
     }
 
-       
-       
-       
-        // document.getElementById("replay").innerHTML ='<button onclick="Replay();">replay</button>';
-
-       
-        
-        // onClick=window.location.reload() `<button>Refresh Page</button>`
-        // location.reload() ` <button onclick="location.reload();">Refresh Page</button>;`
-
   
     chances++;
-
-    
-
-    
-   
+   }
 });
 
 
